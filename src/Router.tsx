eb96aa9a0f6f61c57e97fde67738afd2ae3e2fc6
tnghgks <tnghgks@naver.com>
2023-03-14 21:37:09 +0900

    feat: Home Component 라우팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
 const Router = () => {
-  return <div>Router</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default Router;
