7c20844bc2edd8a63849c37d4a3582d7ca109c9c
jhoon9494 <wjdgns9406@gmail.com>
2023-03-15 16:29:25 +0900

    refactor: 커스텀훅을 사용하여 chart 데이터 전달

--- src/api/chartData.ts
+++ src/api/chartData.ts
@@ -1,18 +1,8 @@
 import apiClient from '@/api';
-import { IData } from '@/interface/chartData';
 
-export const getData = async (url: string) => {
+export const getData = (url: string) => {
   try {
-    const res = await apiClient(url);
-    return Object.entries(res.data.response).map(([date, value]) => {
-      const data = value as IData;
-      return {
-        date,
-        id: data.id,
-        value_area: data.value_area,
-        value_bar: data.value_bar,
-      };
-    });
+    return apiClient.get(url);
   } catch (e) {
     throw new Error('데이터가 없습니다.');
   }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,12 @@
-import { useEffect, useState } from 'react';
-import { getData } from '@/api/chartData';
-import { API_URL } from '@/constants/url';
-import { IChart } from '@/interface/chartData';
-
-import { generateStartAndEndDate } from '@/lib/utils/generateDate';
-import '@/lib/utils/main.css';
 import Chart from '@/components/Chart';
+import useChart from '@/lib/hooks/useChart';
 
 const Home = () => {
-  const [chartData, setChartData] = useState<IChart[]>([]);
-  const { start, end } = generateStartAndEndDate(chartData);
-
-  useEffect(() => {
-    getData(API_URL).then(setChartData).catch(console.error);
-  }, []);
+  const { charts, start, end } = useChart();
 
   return (
     <div className="outer">
-      <Chart data={chartData} start={start} end={end} />
+      <Chart data={charts} start={start} end={end} />
     </div>
   );
 };
