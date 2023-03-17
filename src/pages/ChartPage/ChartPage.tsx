3b64ebcc675177f2548b3c234ff71067d7b81153
tnghgks <tnghgks@naver.com>
2023-03-17 12:38:01 +0900

    refactor: useChart -> useFetch 변경 (커스텀 훅 추상화)
    - 지헌님의 PR[#20] 에서 의견을 적용하였습니다.

--- src/pages/ChartPage/ChartPage.tsx
+++ src/pages/ChartPage/ChartPage.tsx
@@ -1,13 +1,24 @@
 import Chart from '@/components/Chart/Chart';
-import useChart from '@/lib/hooks/useChart';
+import { API_URL } from '@/constants/url';
+import { IChart } from '@/interface/chartData';
+import useFetch from '@/lib/hooks/useFetch';
 import styles from '@/pages/ChartPage/style.module.css';
+import transformData from '@/lib/utils/transformData';
+import { generateStartAndEndDate } from '@/lib/utils/generateDate';
 
 const ChartPage = () => {
-  const { charts, start, end } = useChart();
+  const [chartData, isLoading, isError] = useFetch<IChart[]>(
+    [],
+    API_URL,
+    transformData,
+  );
+  const { startDate, endDate } = generateStartAndEndDate(chartData);
 
+  if (isLoading) return <>Loading...</>;
+  if (isError) return <>Error...</>;
   return (
     <main className={styles.outer}>
-      <Chart data={charts} start={start} end={end} />
+      <Chart data={chartData} start={startDate} end={endDate} />
     </main>
   );
 };
